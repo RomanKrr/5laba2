42767e9a61281cb54f9d06b50620c77da87e3b1a
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2024-05-09 10:26:55 +0300

    first commit

---
+++ css/main.js
@@ -0,0 +1,22 @@
+let qoue = document.querySelector('.par');
+
+function one() {
+    qoue.innerHTML = 'В своїй хаті своя й правда, І сила, і воля.';
+}
+
+function second() {
+    qoue.innerHTML = 'Вітчизна — ось і альфа, і омега!';
+}
+
+function third() {
+    qoue.innerHTML = 'Нації вмирають не від інфаркту. Спочатку їм відбирає мову.';
+}
+
+function four() {
+    qoue.innerHTML = 'Нема на світі України, немає другого Дніпра';
+}
+
+function five() {
+    qoue.innerHTML = 'Ще не вмерла України і слава, і воля';
+}
+
