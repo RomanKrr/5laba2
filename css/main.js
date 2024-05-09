14b3600a0007cf9196d8d004a574c8abd017ee64
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2024-05-09 11:53:44 +0300

    Update main.js

--- css/main.js
+++ css/main.js
@@ -1,22 +1,26 @@
 let qoue = document.querySelector('.par');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
 
-function one() {
+btn1.addEventListener('click', function() {
     qoue.innerHTML = 'В своїй хаті своя й правда, І сила, і воля.';
-}
+});
 
-function second() {
+btn2.addEventListener('click', function() {
     qoue.innerHTML = 'Вітчизна — ось і альфа, і омега!';
-}
+});
 
-function third() {
+btn3.addEventListener('click', function() {
     qoue.innerHTML = 'Нації вмирають не від інфаркту. Спочатку їм відбирає мову.';
-}
+});
 
-function four() {
+btn4.addEventListener('click', function() {
     qoue.innerHTML = 'Нема на світі України, немає другого Дніпра';
-}
+});
 
-function five() {
+btn5.addEventListener('click', function() {
     qoue.innerHTML = 'Ще не вмерла України і слава, і воля';
-}
-
+});
